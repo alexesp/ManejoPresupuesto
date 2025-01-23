b0334f5d1bd22be2c2b31f1d8daa495942864169
alexesp <aleizaes@hotmail.com>
2025-01-23 13:57:45 +0100

    Repo cuentas

---
+++ ManejoPresupuesto/Servicios/RepositorioCuentas.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioCuentas
+    {
+
+    }
+    public class RepositorioCuentas: IRepositorioCuentas
+    {
+        private readonly string _connectionString;
+        public RepositorioCuentas(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(Cuenta cuenta)
+        {
+            using var conn = new SqlConnection(_connectionString);
+            var id = await conn.QuerySingleAsync(@"");
+        }
+    }
+}
